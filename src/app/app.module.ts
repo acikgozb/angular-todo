23dfa5b0448d2025f7c1897b0b7751fc0668ca82
bacikgoz <berk.acikgoz@hepsiburada.com>
2021-04-21 22:58:03 +0300

    created todo container, applied :host selector for component html's, changed several styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoContainerComponent } from './todo-container/todo-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo-container/todo-container.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-container',
+  templateUrl: './todo-container.component.html',
+  styleUrls: ['./todo-container.component.scss']
+})
+export class TodoContainerComponent implements OnInit {
+
+  @HostBinding('class') className = 'todo-container-component';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
